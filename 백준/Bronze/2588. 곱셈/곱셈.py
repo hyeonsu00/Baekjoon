7c61a574b5b5b088c7295f604cbfaa51c7e03f2d
정현수 <117559366+hyeonsu00@users.noreply.github.com>
2023-06-29 16:14:06 +0900

    [Bronze III] Title: 곱셈, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.py
@@ -0,0 +1,9 @@
+num1 = int(input())
+num2 = input()
+
+res3 = num1 * int(num2[2])
+res4 = num1 * int(num2[1])
+res5 = num1 * int(num2[0])
+res6 = num1 * int(num2)
+
+print(res3, res4, res5, res6, sep="\n")
