11564d22eca52cb4b95f4c1e1b0f216575c1c69a
정현수 <117559366+hyeonsu00@users.noreply.github.com>
2023-06-29 16:36:04 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.py
@@ -0,0 +1,12 @@
+#삼항 연산자 코드
+A,B = map(int,input().split())
+print('>') if A>B else print('<') if A<B else print('==')
+
+#if 조건식 코드
+#A,B = map(int, input().split())
+#if A>B:
+#print('>')
+#elif A<B:
+#print('<')
+#else:
+#print('==')
