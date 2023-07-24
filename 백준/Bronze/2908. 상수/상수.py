c082926b67bcbfaa6570e4bfc470c4ff34dcc3fd
정현수 <117559366+hyeonsu00@users.noreply.github.com>
2023-07-24 15:26:25 +0900

    [Bronze II] Title: 상수, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2908. 상수/상수.py
@@ -0,0 +1,11 @@
+num1, num2 = input().split()
+num1 = int(num1[::-1]) #[::-1] -> 역순
+num2 = int(num2[::-1])
+
+if num1>num2:
+    print(num1)
+else:
+    print(num2)
+    
+#삼항 연산자 표현식
+#print(num1) if num1 > num2 else print(num2)
